0a14de18f3f307d4c76f82ffcd9949f8b156bb27
Jin Gang <jingang.li@luckincoffee.com>
2019-01-22 18:15:06 +0800

    增加Kotlin协程相关内容-增加了两种启动协程的方法。
    Signed-off-by: Jin Gang <jingang.li@luckincoffee.com>

--- appKt/src/main/java/com/boy/learning/kotlinlearningtravel/MainActivity.kt
+++ appKt/src/main/java/com/boy/learning/kotlinlearningtravel/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.boy.learning.kotlinlearningtravel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.widget.Switch
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.coroutines.*
 
 /**
  * kotlin定位控件方式
@@ -16,8 +17,11 @@ import kotlinx.android.synthetic.main.activity_main.view.*
  * 1.匿名内部类
  * 2.全局实现OnClickListener接口
  * 3.When表达式(加强版的switch) 支持任意类型 支持纯表达式条件分支（类似于if）
+ *
  */
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG="MainActivity"
+    // ?用来在变量声明出判断是否为null
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_click ->
@@ -40,7 +44,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btn_click2.setOnClickListener(this)
 
+        //协程启动方式一
+        Log.v(TAG," runCoroutineByFirstWay() start")
+        runCoroutineByFirstWay()
+        Log.v(TAG," runCoroutineByFirstWay() end")
+        Log.v(TAG," runCoroutineBySecondWay() start")
+        runCoroutineBySecondWay()
+        Log.v(TAG," runCoroutineBySecondWay()() end")
+        Log.v(TAG,"  runCoroutineByThirdWay() start")
+        runCoroutineByThirdWay()
+        Log.v(TAG,"  runCoroutineByThirdWay() end")
 
+    }
+
+    private fun runCoroutineByThirdWay() {
+
+    }
+
+
+    /**
+     * 用runBlocking方法进行协程启动的时候，当前线程被阻塞
+     * 如果当前协程所在的线程被中断，那么意味着当前因为该协程运行导致的阻塞状态被打断
+     * 那么就会抛出InterruptedException
+     */
+    private fun runCoroutineByFirstWay()= runBlocking {
+        repeat(10) {
+            Log.v(TAG, "Coroutine test runCoroutineByFirstWay() start$it")
+            delay(52)
+        }
+
+
+    }
+
+    /**
+     * 用launch启动一个协程的时候，当前线程不会阻塞
+     * 有点像轻量级线程了，用GlobalScope则代表这个协程生命周期
+     * 跟应用进程是挂钩的，不受控制，所以一般不建议用。
+     */
+    private fun runCoroutineBySecondWay() {
+        GlobalScope.launch(Dispatchers.Default, CoroutineStart.DEFAULT) {
+            delay(1000L)
+            println("Coroutine test runCoroutineBySecondWay()!")
+        }
     }
 
     fun click3(v: View?) {
