319ffa147810f1f91abbc35ced48b12bba6db734
495057454@qq.com <boyi.chen@luckincoffee.com>
2019-01-30 17:29:57 +0800

    【整理】kotlin接口

--- appKt/src/main/java/com/boy/learning/kotlinlearningtravel/KotlinInterfaces.kt
+++ appKt/src/main/java/com/boy/learning/kotlinlearningtravel/KotlinInterfaces.kt
@@ -1,24 +1,23 @@
-package com.lucky.kotlin.luckykotlinlearning
+package com.boy.learning.kotlinlearningtravel
 
 
 /**
  * Kotlin 接口
  * 1.实现接口
  * 2.接口中的属性
- * 3.函数重写
  */
 fun main(args: Array<String>) {
     //-----------------------实现接口-------------------------------------
-    ExtendsMyInterface().A()//ExtendsMyInterface.A
-    ExtendsMyInterface().B()//MyInterface.B
+    ExtendsMyInterface().A()//输出结果-------------------------ExtendsMyInterface.A-------------------------
+    ExtendsMyInterface().B()//输出结果-------------------------MyInterface.B-------------------------
 
     //-----------------------接口中的属性 -------------------------------------
-    println(ExtendsMyInterface2().name)//boyi.chen
+    println(ExtendsMyInterface2().name)//输出结果-------------------------boyi.chen-------------------------
 
 }
-//-----------------------实现接口-------------------------------------
-//一个类或者对象可以实现一个或多个接口。
 
+//-----------------------接口-------------------------------------
+//一个类或者对象可以实现一个或多个接口。
 interface MyInterface {
     fun A()
     fun B() {
@@ -26,11 +25,12 @@ interface MyInterface {
     }
 }
 
+//-----------------------实现接口-------------------------------------
+//一个类或者对象可以实现一个或多个接口。
 class ExtendsMyInterface : MyInterface {
     override fun A() {
         println("ExtendsMyInterface.A")
     }
-
 }
 
 //-----------------------接口中的属性 -------------------------------------
